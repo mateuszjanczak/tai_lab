15928d8cb6e0c6fccebc3ec2972dfc664eed6d46
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-03-10 19:48:33 +0100

    Navbar - reusable component, fixed grid in index.html

---
+++ js/navbar.js
@@ -0,0 +1,18 @@
+let nav = `<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-5">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>`;
+
+let navbarContainer = document.querySelector(".navbar");
+
+navbarContainer.outerHTML = nav;
