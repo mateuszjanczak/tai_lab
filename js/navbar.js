6af4bfe508ba945f61007d92f24291cc88b89ae4
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-03-10 19:59:39 +0100

    General fixes for navbar/footer styles

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@ let nav = `<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-5">
       <div class="collapse navbar-collapse" id="navbarColor02">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+            <a class="nav-link" aria-current="page" href="index.html">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="contact.html">Contact</a>
